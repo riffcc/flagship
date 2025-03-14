c0fcdc49a47fe0c0b16d88cea6600c1e5f9451a1
benya7 <bitup.games@gmail.com>
2025-03-14 18:28:55 +0100

    add: siteId and siteColor

--- packages/renderer/src/components/admin/siteManagement.vue
+++ packages/renderer/src/components/admin/siteManagement.vue
@@ -4,6 +4,39 @@
       class="px-6 py-4 mx-auto"
       max-width="448px"
     >
+      <v-list-item class="px-0 mb-4">
+        <template #prepend>
+          <v-menu>
+            <template #activator="{ props }">
+              <v-btn
+                v-bind="props"
+                icon="mdi-circle"
+                variant="text"
+                density="compact"
+                size="x-small"
+                class="mr-2"
+                :color="getSiteColor(orbiter.siteId)"
+              />
+            </template>
+            <v-color-picker
+              v-model="selectedColors[orbiter.siteId]"
+              @update:model-value="saveColor(orbiter.siteId, $event)"
+            />
+          </v-menu>
+        </template>
+
+        <p class="text-body">Site ID: {{ `${orbiter.siteId.slice(0, 17)}...${orbiter.siteId.slice(-10)}` }}</p>
+        <template #append>
+          <v-btn
+            icon="mdi-clipboard-multiple-outline"
+            variant="text"
+            density="comfortable"
+            size="small"
+            @click="copyText(orbiter.siteId)"
+          ></v-btn>
+        </template>
+      </v-list-item>
+
       <h6 class="text-h6 font-weight-bold mb-4">Edit Site Info</h6>
       <v-file-input
         v-model="file"
@@ -54,6 +87,9 @@
 <script setup lang="ts">
 import { type Ref, ref, watch } from 'vue';
 import { IPFS_GATEWAY } from '/@/constants/ipfs';
+import { useOrbiter } from '/@/plugins/orbiter/utils';
+import { useSiteColors } from '/@/composables/siteColors';
+import { copyText } from '/@/utils';
 
 const file: Ref<File | null> = ref(null);
 const fileBlobUrl: Ref<string | null> = ref(null);
@@ -71,4 +107,6 @@ watch(file, (v) => {
 });
 
 function handleOnSave(){};
+const {orbiter} = useOrbiter();
+const {getSiteColor, saveColor, selectedColors} = useSiteColors();
 </script>
