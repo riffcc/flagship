729e83d53a4bb43efb94adea76eeebd4fed27200
julienmalard <julien.malard@mail.mcgill.ca>
2025-04-23 09:31:22 +0200

    Veuillez saisir le message de validation pour vos modifications. Les lignes

--- packages/main/src/constellation.ts
+++ packages/main/src/constellation.ts
@@ -1,17 +1,27 @@
 import {GestionnaireFenêtres} from '@constl/mandataire-electron-principal';
+import {RIFFCC_PROTOCOL} from '/@/consts';
 
 const enDéveloppement = import.meta.env.DEV;
 
 const importationIPA = import('@constl/ipa');
 const importationServeur = import('@constl/serveur');
 
-// Avec ça, on peut spécifier le dossier Constellation dans les tests
-const opts = process.env.DOSSIER_CONSTL ? {dossier: process.env.DOSSIER_CONSTL} : undefined;
 
 export const gestionnaireFenêtres = new GestionnaireFenêtres({
   enDéveloppement,
   importationIPA,
   importationServeur,
   journal: enDéveloppement ? console.log : undefined,
-  opts,
+  opts: {
+    protocoles: [RIFFCC_PROTOCOL], 
+
+    // Useful for running tests with a temporary folder
+    dossier: process.env.DOSSIER_CONSTL,
+
+    // Add custom domains (only for peer reachable on this domain)
+    domaines: process.env.VITE_DOMAINS?.split(',').map(d=>d.trim()),
+
+    // Add custom peers (other peers to connect to automatically)
+    pairsParDéfaut: process.env.VITE_BOOTSTRAPPERS?.split(',').map(b=>b.trim()),
+  },
 });
--- packages/renderer/src/plugins/constellation/browser.ts
+++ packages/renderer/src/plugins/constellation/browser.ts
@@ -3,9 +3,20 @@ import type {App} from 'vue';
 import {mandataire} from '@constl/ipa';
 import {RIFFCC_PROTOCOL} from '/@/utils';
 
+const DOMAINS = process.env.VITE_DOMAINS?.split(',').map(d=>d.trim());
+
+
 export default {
   install: (app: App) => {
-    const client = mandataire.générerMandataireProc({protocoles: [RIFFCC_PROTOCOL]});
+    const client = mandataire.générerMandataireProc({
+      protocoles: [RIFFCC_PROTOCOL], 
+      
+      // Add custom domains (only for peer reachable on this domain)
+      domaines: DOMAINS,
+      
+      // Add custom peers (other peers to connect to automatically)
+      pairsParDéfaut: process.env.VITE_BOOTSTRAPPERS?.split(',').map(b=>b.trim()),
+    });
     app.config.globalProperties.$constl = client;
     app.provide('constl', client);
   },
