5fce02f2367288d9c48f8d52d47f950aa6733a55
Benjamin Arntzen <zorlin@gmail.com>
2025-05-24 18:09:02 +0100

    Switch to mdi Vue package instead of Woff

--- packages/renderer/src/plugins/vuetify.ts
+++ packages/renderer/src/plugins/vuetify.ts
@@ -5,18 +5,96 @@
  */
 
 // Styles
-import '@mdi/font/css/materialdesignicons.css';
 import colors from 'vuetify/util/colors';
 import '/@/styles/main.scss';
 import '/@/styles/settings.scss';
 // Composables
 import {createVuetify} from 'vuetify';
-import {aliases, mdi} from 'vuetify/iconsets/mdi';
+import {mdi} from 'vuetify/iconsets/mdi-svg';
+// Import only the icons we use
+import {
+  mdiAccount,
+  mdiAccountSupervisor,
+  mdiArrowLeft,
+  mdiBlockHelper,
+  mdiCheck,
+  mdiChevronUp,
+  mdiCircle,
+  mdiClipboardCheckMultipleOutline,
+  mdiClipboardMultipleOutline,
+  mdiClose,
+  mdiDelete,
+  mdiDotsVertical,
+  mdiDownload,
+  mdiFullscreen,
+  mdiHeart,
+  mdiHelpCircleOutline,
+  mdiMenu,
+  mdiMenuLeft,
+  mdiMenuRight,
+  mdiPause,
+  mdiPauseCircle,
+  mdiPencil,
+  mdiPlay,
+  mdiPlayCircle,
+  mdiPlus,
+  mdiPlusCircle,
+  mdiRotateLeft,
+  mdiShareVariant,
+  mdiShuffle,
+  mdiSkipNext,
+  mdiSkipPrevious,
+  mdiStar,
+  mdiStarPlusOutline,
+  mdiSvg,
+  mdiVolumeHigh,
+  mdiVolumeOff,
+} from '@mdi/js';
+
+// Create custom aliases with only the icons we use
+const customAliases = {
+  account: mdiAccount,
+  accountSupervisor: mdiAccountSupervisor,
+  arrowLeft: mdiArrowLeft,
+  blockHelper: mdiBlockHelper,
+  check: mdiCheck,
+  chevronUp: mdiChevronUp,
+  circle: mdiCircle,
+  clipboardCheckMultipleOutline: mdiClipboardCheckMultipleOutline,
+  clipboardMultipleOutline: mdiClipboardMultipleOutline,
+  close: mdiClose,
+  delete: mdiDelete,
+  dotsVertical: mdiDotsVertical,
+  download: mdiDownload,
+  fullscreen: mdiFullscreen,
+  heart: mdiHeart,
+  helpCircleOutline: mdiHelpCircleOutline,
+  menu: mdiMenu,
+  menuLeft: mdiMenuLeft,
+  menuRight: mdiMenuRight,
+  pause: mdiPause,
+  pauseCircle: mdiPauseCircle,
+  pencil: mdiPencil,
+  play: mdiPlay,
+  playCircle: mdiPlayCircle,
+  plus: mdiPlus,
+  plusCircle: mdiPlusCircle,
+  rotateLeft: mdiRotateLeft,
+  shareVariant: mdiShareVariant,
+  shuffle: mdiShuffle,
+  skipNext: mdiSkipNext,
+  skipPrevious: mdiSkipPrevious,
+  star: mdiStar,
+  starPlusOutline: mdiStarPlusOutline,
+  svg: mdiSvg,
+  volumeHigh: mdiVolumeHigh,
+  volumeOff: mdiVolumeOff,
+};
 // https://vuetifyjs.com/en/introduction/why-vuetify/#feature-guides
 const vuetify = createVuetify({
   icons: {
     defaultSet: 'mdi',
-    aliases,
+    aliases: customAliases,
     sets: {
       mdi,
     },
